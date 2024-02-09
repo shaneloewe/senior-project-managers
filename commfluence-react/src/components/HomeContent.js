5fcb9786ef549393eae858f7d02d18dbc356efe3
loeweshane <loewe.shane@gmail.com>
2024-02-08 17:39:18 -0700

    styled documents page

--- commfluence-react/src/components/HomeContent.js
+++ commfluence-react/src/components/HomeContent.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Authentication from '../components/auth/Authentication';
 import AuthDetails from '../components/auth/AuthDetails';
+import Header from '../components/Header.js';
+import '../styles/Header.css'
 
 const Home = () => {
     return (
 
-        <div className="App">
+        <div className="home-page">
+            <Header />
             <h2>Let's get<br></br>started</h2>
             <Authentication />
             <AuthDetails />
