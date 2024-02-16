289814e90629d27cf5bc13ee12ed52156d2a1bc1
loeweshane <loewe.shane@gmail.com>
2024-02-15 17:23:58 -0700

    Revert "build thing"
    This reverts commit 3a721e6cdb9fb78d5ba0255f3c0e31fa56effbd6.

--- commfluence-react/src/App.js
+++ commfluence-react/src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Projects from './pages/Projects';
 import "./styles/Header.css";
 import Header from "./components/Header";
 import Home from "./components/HomeContent";
 import CreateDocument from './components/CreateDocument';
 import DocumentPage from './components/DocumentPage';
 import DocumentViewer from './components/DocumentViewer';
-
 
 
 const App = () => (
@@ -22,7 +20,6 @@ const App = () => (
         <Route path="/create-document" element={<CreateDocument />} />
         <Route path="/documents" element={<DocumentPage />} />
         <Route path="/document/:docId" element={<DocumentViewer />} />
-        <Route path="/projects" element={<Projects />} />
       </Routes>
     </Router>
   </div>
