e6df9cd94789d46e0a1d2482171d5251be32e4c8
cohub77 <hubb2447@bears.unco.edu>
2024-03-21 18:25:06 -0600

    css gogogo

--- commfluence-react/src/components/UserListPopup.js
+++ commfluence-react/src/components/UserListPopup.js
@@ -11,18 +11,24 @@ const UserListPopup = ({ users, onAddUser, onClose }) => {
   return (
     <div className="popup-overlay">
       <div className="popup-content">
-        <button onClick={onClose} className="close-popup">X</button>
-        <ul>
+        <button onClick={onClose} className="close-popup">←</button>
+        <div className="popup-title">Collaborators</div>
+        <ul className="user-list">
           {users.map((user, index) => (
-            <li key={index}>{user}</li> 
+            <li key={index} className="user-circle">{user.charAt(0).toUpperCase()}</li>
           ))}
         </ul>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter user email"
-        />
+
+        <div className="email-input-container">
+          <input
+            className="email-input" // Added class for styling
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter user email"
+          />
+        </div>
+
         <button onClick={handleAddUser}>Add User</button>
       </div>
     </div>
