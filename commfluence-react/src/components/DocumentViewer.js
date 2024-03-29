b7f66c287afcb0cdb2e843cda5f83baf16a99de6
cohub77 <hubb2447@bears.unco.edu>
2024-03-28 18:12:56 -0600

    fix toolbar

--- commfluence-react/src/components/DocumentViewer.js
+++ commfluence-react/src/components/DocumentViewer.js
@@ -27,8 +27,30 @@ const DocumentViewer = () => {
 
   useEffect(() => {
     if (document) {
+      const toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+        ['blockquote', 'code-block'],
+  
+        [{ 'header': 1 }, { 'header': 2 }], // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }], // superscript/subscript
+        [{ 'indent': '-1'}, { 'indent': '+1' }], // outdent/indent
+        [{ 'direction': 'rtl' }], // text direction
+  
+        [{ 'size': ['small', false, 'large', 'huge'] }], // custom dropdown
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+        [{ 'color': [] }, { 'background': [] }], // dropdown with defaults
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+  
+        ['clean'] // remove formatting button
+      ];
       quillInstance.current = new Quill(quillRef.current, {
         theme: 'snow',
+        modules: {
+          toolbar: toolbarOptions
+        }
       });
 
       const content = JSON.parse(document.content); // Parse the JSON string
