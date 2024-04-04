1dd5737b3ea40fc535ec892247111c8eb1a8be84
Eriku121 <122395587+Eriku121@users.noreply.github.com>
2024-04-04 07:06:27 +0000

    new file:   commfluence-react/src/pages/Help.js

---
+++ commfluence-react/src/pages/Help.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../styles/App.css';
+import '../styles/LandingPage.css';
+import '../styles/Header.css';
+import Header from '../components/Header.js';
+
+
+function Contact() {
+    return (
+        <div className="home-page">
+            <Header />
+            <h2>FAQ</h2>
+            <p className="Help-text">Questions?</p>
+            
+        </div>
+    );
+}
+
+export default Contact;
